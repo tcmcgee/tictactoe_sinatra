fcf225e5b4744b633eb067b963e355f8f83f3cb5
tcmcgee <tmmcgee13@gmail.com>
2014-07-31 08:31:16 -0500

    Working board

--- mygame.rb
+++ mygame.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
-require './lib/presenters/ttt_option_presenter'
 
 set :public_folder, 'public'
 
 get '/hello_world' do
   redirect 'images/twitter.gif'
+end
+
+get '/' do
+  erb :board
 end
